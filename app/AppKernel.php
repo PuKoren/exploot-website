a95470c26186742897b3160ff99ccae7c915362c
PuKoren <koren@pukogames.com>
2014-05-15 22:01:51 +0200

    Working background image in css with assetics using plugin https://github.com/fkrauthan/FkrCssURLRewriteBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Exploot\Front\WebsiteBundle\ExplootFrontWebsiteBundle(),
+            new Fkr\CssURLRewriteBundle\FkrCssURLRewriteBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
